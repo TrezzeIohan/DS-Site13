5b52b90a5da3e7793c1574ec6ff25582192265c5
Iohan R. Trézze <iohan.trezze@gmail.com>
2023-03-02 12:09:05 -0300

    Mudanças no HTML, CSS e JS
    Mudanças na estrutura da html e CSS necessárias para que a página se encaixasse com a palheta de cores passada pelo cliente. Mudanças na fórmula de mudança de cor de background. Agora a mudança acontece mais rápido.

---
+++ custom.js
@@ -0,0 +1,63 @@
+
+  (function ($) {
+
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    // CUSTOM LINK
+    $('.custom-link').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height() + 10;
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+  }
+
+});
+
+  })(window.jQuery);
+
+
+  $(window).scroll(function() {
+
+    // selectors
+    var $window = $(window),
+        $body = $('body'),
+        $panel = $('.panel');
+
+    // Change 33% earlier than scroll position so colour is there when you arrive.
+    var scroll = $window.scrollTop() + ($window.height() / 1.2);
+
+    $panel.each(function () {
+      var $this = $(this);
+
+      // if position is within range of this panel.
+      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+      // Remember we set the scroll to 33% earlier in scroll var.
+      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+        // Remove all classes on body with color-
+        $body.removeClass(function (index, css) {
+          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+        });
+
+        // Add class of currently active div
+        $body.addClass('color-' + $(this).data('color'));
+      }
+    });
+
+  }).scroll();
